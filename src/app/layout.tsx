dc4abac03f23ded8598a920639305be02a9d4646
tommyjohncodes <tom@ciccotta.com>
2026-03-15 02:35:56 +0000

    Mar 14, 2026, 10:35 PM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Public_Sans } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
 import { TRPCReactProvider } from "@/trpc/client";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const publicSans = Public_Sans({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-mono",
   subsets: ["latin"],
 });
 
@@ -45,7 +45,7 @@ export default function RootLayout({
       <TRPCReactProvider>
         <html lang="en" suppressHydrationWarning>
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${publicSans.variable} ${geistMono.variable} antialiased font-sans`}
           >
             <ThemeProvider
               attribute="class"

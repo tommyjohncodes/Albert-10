82065a3bed38d59cd6549cade1f16bba25fe0381
tommyjohncodes <tom@ciccotta.com>
2026-03-15 03:50:43 +0000

    Mar 14, 2026, 11:50 PM

--- src/components/user-control.tsx
+++ src/components/user-control.tsx
@@ -2,9 +2,11 @@
 
 import { dark } from "@clerk/themes";
 import { UserButton } from "@clerk/nextjs";
+import { CloudUpload } from "lucide-react";
 
 import { useCurrentTheme } from "@/hooks/use-current-theme";
 import { UserMetrics } from "@/components/user-metrics";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   showName?: boolean;
@@ -15,18 +17,29 @@ export const UserControl = ({ showName }: Props) => {
 
   return (
     <div className="flex items-center gap-3">
+      <div className="flex items-center gap-2">
+        <Button variant="outline" size="sm">
+          Share
+        </Button>
+        <Button size="sm">
+          <CloudUpload />
+          Publish
+        </Button>
+      </div>
       <UserMetrics />
-      <UserButton
-        showName={showName}
-        appearance={{
-          elements: {
-            userButtonBox: "rounded-md!",
-            userButtonAvatarBox: "rounded-md! size-8!",
-            userButtonTrigger: "rounded-md!"
-          },
-          baseTheme: currentTheme === "dark" ? dark : undefined,
-        }}
-      />
+      <div className="ml-auto">
+        <UserButton
+          showName={showName}
+          appearance={{
+            elements: {
+              userButtonBox: "rounded-md!",
+              userButtonAvatarBox: "rounded-md! size-8!",
+              userButtonTrigger: "rounded-md!",
+            },
+            baseTheme: currentTheme === "dark" ? dark : undefined,
+          }}
+        />
+      </div>
     </div>
   );
 };
